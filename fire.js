435df944bba9580e297ac2949cac353e5fa01a69
sahak7an <sahak7anerik@gmail.com>
2022-03-12 14:13:32 +0400

    das 2

--- fire.js
+++ fire.js
@@ -1,7 +1,5 @@
 class Fire {
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
+    constructor(x, y,index){
         this.energy = 10;
         this.directions = [];
         this.mulEnergy = 100;
